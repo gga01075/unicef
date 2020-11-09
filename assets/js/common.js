aaafe1941939723656a431497987eba51c2132fb
gga01075 <72422020+gga01075@users.noreply.github.com>
2020-11-09 18:03:08 +0900

    201109 유니세프 gnb수정

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     var _gnb = $('#gnb');
+    var _dep1a = $('#gnb>ul>li>a');
+    var _dep2a = $('.depth2>li>a');
 
+    //햄버거 메뉴 클릭
     $('.btn_menu').on('click',function(){
         if($(this).hasClass('active')){ //gnb닫을때
             $(this).removeClass('active').find('.blind_b').text('메뉴 열기');
@@ -8,19 +11,42 @@ $(document).ready(function(){
             _gnb.stop().animate({left: '100%'}, 300, function () {
                 $(this).css({display:'none'});
               });
+            _dep1a.show();
+            $('#gnb>ul>li>ul').hide().css({left:0}).removeClass('on').find('>li').removeClass().find('ul').hide();  
+
+
         }else{                          //gnb열때
             $(this).addClass('active').find('.blind_b').text('메뉴 닫기');
             _gnb.css({display:'block'}).stop().animate({left:0},function(){
                 
        });
+       }
+    });
 
+    //#gnb depth1 a클릭
+    _dep1a.on('click',function(){
+        var depth1Txt = $(this).text();
+        $(this).next().show().animate({left:'-70px'},'fast');
+        $('#gnb>ul>li>a').css({display:'none'});
+        $(this).next().addClass('on');
+    });
 
-
-        
-              
-
+    //#gnb depth2 a클릭
+    _dep2a.on('click',function(){
+        if($(this).next().length===0){
+            location.href = $(this).attr('href');
+        }else{
+            $(this).parent().siblings().removeClass('dep3_on').find('ul').stop().slideUp();
+            $(this).next().stop().slideToggle().parent().toggleClass('dep3_on');
+            return false;
         }
     });
 
+    //#footer Top버튼 클릭 애니메이션
+    $('#footer #topBtn').on('click',function(){
+        $('html,body').stop().animate({scrollTop:0});
+
+        return false;
+    });
 
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-    var mySwiper1 = new Swiper('.slogan .swiper-container', {
+   
+  //슬로건
+  var mySwiper1 = new Swiper('.slogan .swiper-container', {
         // Optional parameters
         loop: true,
         effect:'fade',
@@ -15,7 +17,7 @@ $(document).ready(function(){
 
       });
 
-
+    //뉴스
     var mySwiper2 = new Swiper('.news .swiper-container', {
 
         pagination: {
@@ -23,10 +25,39 @@ $(document).ready(function(){
             type: 'bullets',
             clickable:true,
         },
-
-
       });
 
 
+    
+      //유챔프
 
+      //유챔프9기 마감기간까지 남은 날짜 계산하는 함수
+      function uchampDeadline(){  
+        var now = new Date();
+        var nowYy = now.getFullYear();
+  
+        var dDay = new Date(nowYy,10,25);
+  
+        var remain = dDay - now;
+        var remainDay = Math.floor(remain/(1000*60*60*24));   
+        remain %= (1000*60*60*24);
+        var remainHor = Math.floor(remain/(1000*60*60));
+        if(remainHor < 10) remainHor= '0'+remainHor;
+        remain %= (1000*60*60);
+        var remainMin = Math.floor(remain/(1000*60));
+        if(remainMin < 10) remainMin= '0'+remainMin;
+        remain %= (1000*60);
+        var remainSec = Math.floor(remain/(1000));
+        if(remainSec < 10) remainSec= '0'+remainSec;
+
+        $('.uchamp_recruit p:last-of-type').text(remainDay + ' Days '+ remainHor +' hours '+ remainMin + ' minutes '+ remainSec +' seconds ');
+      }
+
+      uchampDeadline();
+
+      var timer = setInterval(uchampDeadline,1000);
+
+
+
+      
 });
